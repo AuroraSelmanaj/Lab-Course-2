2d657268d8e1293245a9f6b4b4c911f2220cea90
aurora <147750558+AuroraSelmanaj@users.noreply.github.com>
2026-01-19 19:00:43 +0100

    added docker intructions for test

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -1,11 +1,12 @@
 import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
+const port: number = 4000;
 
 app.get('/api', (req: Request, res: Response) => {
     res.json({ test: "response is okay" })
 });
 
-app.listen(4000, () => {
-    console.log(`Listening port 4000`);
+app.listen(port, () => {
+    console.log(`Listening port ${port}`);
 })
