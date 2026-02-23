d96cc58e7920318214086e25417f534145a75c43
aurora <147750558+AuroraSelmanaj@users.noreply.github.com>
2026-02-23 20:21:42 +0100

    added extra validation before creating course

--- api-gateway/src/routes/course.routes.ts
+++ api-gateway/src/routes/course.routes.ts
@@ -1,4 +1,4 @@
-import Router, { NextFunction, Request, Response } from "express";
+import Router, { Request, Response } from "express";
 import axios from "axios";
 
 const courseRoutes = Router();
@@ -9,11 +9,40 @@ const secret = {
     }
 }
 
-courseRoutes.get('/course', async (req: Request, res: Response, next: NextFunction) => {
-    // const response = await axios.post(`http://localhost:4001/signIn`, req.body, secret);
+courseRoutes.get('/course', async (req: Request, res: Response) => {
     const response = await axios.get(`http://localhost:4002/`, secret);
-
     res.send(response.data);
+})
+
+courseRoutes.post('/course/create', async (req: Request, res: Response) => {
+
+    try {
+        const token = req.headers.authorization;
+
+        const validation = await axios.post(
+            "http://localhost:4001/validate",
+            {},
+            {
+                headers: { Authorization: token, ...secret.headers }
+            }
+        );
+        
+        const response = await axios.post(
+            "http://localhost:4002/",
+            req.body,
+            {
+                headers: {
+                    "x-user-data": JSON.stringify(validation.data.user),
+                    ...secret.headers
+                }
+            }
+        );
+
+        res.json(response.data);
+
+    } catch (error: any) {
+        res.status(401).json({ message: "Unauthorized" });
+    }
 })
 
 
--- services/auth/src/routes/auth.routes.ts
+++ services/auth/src/routes/auth.routes.ts
@@ -31,8 +31,6 @@ router.post("/signUp", async (req: Request, res: Response) => {
 router.post("/signIn", async (req: Request, res: Response) => {
   try {
 
-    console.log("po hin");
-    
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
@@ -59,5 +57,30 @@ router.post("/signIn", async (req: Request, res: Response) => {
     res.status(500).json({ message: "Server error" });
   }
 });
+
+
+router.post("/validate", (req: Request, res: Response) => {
+
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ valid: false });
+  }
+
+  try {
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    );
+    
+    return res.json({
+      valid: true,
+      user: decoded
+    });
+
+  } catch {
+    return res.status(401).json({ valid: false });
+  }
+})
 
 export default router;
